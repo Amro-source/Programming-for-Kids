ec8742fd30627c2beb409b2cb07e49e650021719
Amro <68817735+Amro-source@users.noreply.github.com>
2021-04-18 17:32:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationYoussef1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int X = 10;
+            //int Y = 30;
+
+
+
+            //int W = 2 * X + 5 * Y;
+
+
+            //Console.WriteLine("Youssef made me a cup of milk tea");
+
+
+            //Console.WriteLine("X= {0}",X);
+            //Console.WriteLine("Y= {0}",Y);
+
+
+            //Console.WriteLine("W={0}",W);
+
+            //Console.ReadKey();
+
+
+
+            int X = 100;
+            int Y = 1000;
+
+
+            int A = 20 * X + 60 * Y;
+
+
+            Console.WriteLine("Youssef is learning programing");
+
+
+            Console.WriteLine("X= {0}", X);
+            Console.WriteLine("Y= {0}", Y);
+
+            Console.WriteLine("A={0}", A);
+
+            Console.ReadKey();
+        }
+    }
+}
